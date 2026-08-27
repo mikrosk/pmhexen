13e96085812a5e37a936582c04651734abfa2648
Miro Kropacek <miro.kropacek@gmail.com>
2026-08-27 11:08:00 +1000

    Fix autotools stuff

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+# Regenerate the build system. Stop at the first failure so errors
+# don't cascade into misleading follow-up messages.
+set -e
 
-#aclocal
-aclocal -I m4 -I /usr/local/share/aclocal
-automake --add-missing --copy
+# automake copies its helper scripts here but won't create the directory.
+mkdir -p autotools
+
+aclocal -I m4
 autoheader
+automake --add-missing --copy
 autoconf
